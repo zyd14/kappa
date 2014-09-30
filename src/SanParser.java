ee1fd0a2be6a51dbd17e355d23b13a9760c73d9a
BallroomProgrammer <houyubing24@hotmail.com>
2014-09-30 09:55:03 -0500

    Improved .sam file parser

--- src/SanParser.java
+++ src/SanParser.java
@@ -3,100 +3,98 @@ import java.util.Scanner;
 
 public class SanParser {
 
-    private static int left;
-    private static int right;
-    private static int bpchain [];
+    private int left;
+    private int right;
+    private int [] bpchain;
 
-    public static void addStartEnd (String line, int startColumn, int endColumn) {
-        String [] elements = line.split(" ");
+    private void addStartEnd (String line) {
 
-        int start, end;
+        int seq = line.indexOf("seq");
+        int end = line.indexOf("M\t");
+        if (seq < 0 || end < 0) {
+            System.out.println("Inconsistent Data Formant. Line omitted.");
+            return;
+        }
+        String clean = line.substring(seq, end);
+        String [] elements = clean.split("\t");
+
+        int start, len;
         try {
-            start = Integer.parseInt(elements[startColumn]);
-            end = Integer.parseInt(elements[endColumn]);
-        } catch (NumberFormatException e) {
-            System.out.println("<Program SanParser> Number Format Error");
-            System.out.println ("Action Aborted.");
+            start = Integer.parseInt(elements[1]);
+            len = Integer.parseInt(elements[elements.length - 1]);
+        } catch (Exception e) {
+            System.out.printf("Inconsistent Data Format. Line %s omitted.\n", line);
             return;
         }
 
-        for (int i = start; i < end; i++) {
-            bpchain[i] = bpchain[i] + 1;
+        for (int i = start; i < start + len; i++) {
+            this.bpchain[i] += 1;
         }
     }
 
-    public static void main (String [] args) {
+    public void parse (String [] args) {
 
         if (args.length != 5) {
-            System.out.println ("<Program SanParser> args[0] INFILE, args[1] N, args[2] COL_START, args[3] COL_END, args[4] OUTFILE");
-            System.out.println ("Action Aborted.");
+            System.out.println ("<Program SanParser Arguments>");
+            System.out.println ("\targs[0] Input file path/name.sam");
+            System.out.println ("\targs[1] Length of contig [Unit: bp]");
+            System.out.println ("\targs[2] Starting line number");
+            System.out.println ("\targs[3] Ending line number");
+            System.out.println ("\targs[4] Output file path/name.csv");
             return;
         }
 
-        Scanner scnr = new Scanner (System.in);
         String inFilePath = args[0];
         String outFilePath = args[4];
-        int n, startColumn, endColumn;
+        int n, begin, end;
 
         try {
             n = Integer.parseInt(args[1]);
+            begin = Integer.parseInt(args[2]);
+            end = Integer.parseInt(args[3]);
         } catch (Exception e) {
-            System.out.println ("<Program SanParser> Bad param N");
-            System.out.println ("Action Aborted.");
-            return;
-        }
-
-        try {
-            startColumn = Integer.parseInt(args[2]);
-        } catch (Exception e) {
-            System.out.println ("<Program SanParser> Bad param COL_START");
-            System.out.println ("Action Aborted.");
-            return;
-        }
-
-        try {
-            endColumn = Integer.parseInt(args[3]);
-        } catch (Exception e) {
-            System.out.println ("<Program SanParser> Bad param COL_END");
-            System.out.println ("Action Aborted.");
-            return;
-        }
-
-        if (startColumn >= endColumn) {
-            System.out.println ("<Program SanParser> Bad param: COL_START >= COL_END");
-            System.out.println ("Action Aborted.");
+            System.out.println ("<Program SanParser> Bad paramenter. Action aborted.");
             return;
         }
 
         File inFile = new File (inFilePath);
         File outFile = new File (outFilePath);
-        bpchain = new int [n];
+        this.bpchain = new int [n];
 
         try {
-            Scanner fileScnr = new Scanner (inFile);
-            while (fileScnr.hasNextLine ()) {
-                String line = fileScnr.nextLine();
-                addStartEnd (line, startColumn, endColumn);
+            Scanner scnr = new Scanner (inFile);
+
+            // Skip lines
+            for (int i = 1; i < begin; i++) {
+                scnr.nextLine();
             }
-            fileScnr.close();
-        } catch (FileNotFoundException e1) {
-            System.out.println("<Program SanParser> Invalid file information.");
+
+            // Read lines
+            for (int i = begin; i <= end; i++) {
+                String line = scnr.nextLine();
+                this.addStartEnd (line);
+            }
+            scnr.close();
+        } catch (IOException e) {
+            System.out.println("<Program SanParser> I/O error. Action aborted.");
         }
 
         try {
             PrintWriter writer = new PrintWriter(outFile);
             for (int i = 0; i < n; i++) {
-                writer.println(i + "," + bpchain[i]);
+                writer.println(i + "," + this.bpchain[i]);
             }
             writer.close();
-        } catch (FileNotFoundException e1) {
-            System.out.println("<Program SanParser> Invalid file information.");
+        } catch (IOException e) {
+            System.out.println("<Program SanParser> I/O error. Action aborted");
             for (int i = 0; i < n; i++) {
                 System.out.println(i + "," + bpchain[i]);
             }
         }
+    }
 
-
-        scnr.close ();
+    public static void main (String [] args) {
+        SanParser sam = new SanParser();
+        sam.parse(args);
     }
 }
